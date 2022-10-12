514d35c2439ab0d5e1abfa8e856e00e0428f11d1
IG <iikim2511@gmail.com>
2022-10-12 10:21:57 -0400

    gitignore

--- server.js
+++ server.js
@@ -10,17 +10,26 @@ var expressLayouts = require('express-ejs-layouts');
 // dotenv
 const dotenv = require('dotenv')
 
+// express-ejs-layouts
+var expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
+
+// static
+app.use(express.static('public'));
+
+// router
+// const routes = require('./server/routes/recipeRoutes.js');
+// app.use('/',routes);
+
 
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
-
-
-
-
+// listen
 const port = process.env.PORT || 8080;
 app.listen(port, ()=>{
-    console.log(('ig-listen').bgMagenta)
+    console.log((`ig-listen ${port}`).bgMagenta)
 })
